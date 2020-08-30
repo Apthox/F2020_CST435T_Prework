1bcd5c5cfd4b451941c45cb9f40553576fa36377
Apthox <kevin@apthos.net>
2020-08-30 14:02:44 -0700

    Initial Commit

---
+++ Tip_Prework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip_Prework
+//
+//  Created by Kevin Guzman on 8/30/20.
+//  Copyright © 2020 Kevin Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tip_PreworkTests/Tip_PreworkTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tip_PreworkTests.swift
+//  Tip_PreworkTests
+//
+//  Created by Kevin Guzman on 8/30/20.
+//  Copyright © 2020 Kevin Guzman. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_Prework
+
+class Tip_PreworkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
