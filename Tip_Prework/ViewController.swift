8724754c40742231f6e1fc5a50cfe3d7d9267ea2
Apthox <kevin@apthos.net>
2020-08-30 18:42:23 -0700

    updated

--- Tip_Prework/ViewController.swift
+++ Tip_Prework/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipPercent: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onBillChange(_ sender: Any) {
+        print("Updated")
+        let bill = Double(billField.text!) ?? 0
+        
+        let tipPercentages = [0.1, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipPercent.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
--- readme.md
+++ readme.md
@@ -28,7 +28,7 @@ The following **additional** features are implemented:
 
 Here's a walkthrough of implemented user stories:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://s8.gifyu.com/images/ezgif.com-crop181ab632ab4c14ea.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
 
